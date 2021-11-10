fade5d4d98866d6d60d4ff24a304dd2469823bba
angel samanez <angel.samanez@tecsup.edu.pe>
2021-11-10 11:51:28 -0500

    animacion

--- lib/Login/login.dart
+++ lib/Login/login.dart
@@ -13,3 +13,20 @@ class LoginPage extends StatelessWidget {
     );
   }
 }
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => LoginPage(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
